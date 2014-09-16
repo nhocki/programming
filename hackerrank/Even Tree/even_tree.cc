dbf5790e7c2ff4168d1530c6f05d7b89f0a4d24a
Nicolás Hock Isaza <nhocki@gmail.com>
2014-09-15 18:43:46 -0700

    Hacker Rank - Even tree

---
+++ hackerrank/Even Tree/even_tree.cc
@@ -0,0 +1,74 @@
+#include <algorithm>
+#include <iostream>
+#include <iterator>
+#include <sstream>
+#include <fstream>
+#include <cassert>
+#include <cstdlib>
+#include <cstring>
+#include <climits>
+#include <cctype>
+#include <cstdio>
+#include <vector>
+#include <string>
+#include <cmath>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <list>
+#include <map>
+#include <set>
+
+#define D(x) cout << __LINE__ <<"   "<<#x"  is  " << x << endl
+#define sz size()
+#define lng length()
+#define cs c_str()
+
+using namespace std;
+
+int N, M;
+vector<int> g[150];
+
+int countNodes(int node){
+  int result = 1;
+
+  for(int i=0; i<g[node].size(); ++i)
+    result += countNodes(g[node][i]);
+
+  return result;
+}
+
+int f(int root){
+  int result = 0;
+
+  queue<int> q;
+  q.push(root);
+
+  while(q.size()){
+    int n = q.front();
+    q.pop();
+
+    for(int i = 0; i<g[n].size(); ++i){
+      int node = g[n][i];
+      if(countNodes(node) % 2 == 0){ result++; }
+      q.push(node);
+    }
+  }
+  return result;
+}
+
+int main(){
+  while(scanf("%d%d", &N, &M) == 2){
+    for(int i=0; i<= N + 2; ++i) g[i].clear();
+
+    for(int i=0; i<M; ++i){
+      int u,v;
+
+      scanf("%d%d", &u, &v);
+      g[v].push_back(u);
+    }
+
+    printf("%d\n", f(1));
+  }
+  return 0;
+}
